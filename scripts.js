4dc1ae3fb9c66637e090650c9e1983350ec3c152
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-05-09 16:30:51 +0200

    Adding error messages for different scenarios

--- scripts.js
+++ scripts.js
@@ -16,5 +16,26 @@ form.addEventListener("submit", (event) => {
     divideResult = Math.floor(divideResult);
   };
 
+  if ( dividend === "" || divider === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  };
+
+  if ( isNaN(dividend) || isNaN(divider) ) {
+    console.error("Invalid input values", new Error("Input values should be numbers.") );
+    result.classList.add("critical-error");
+    result.innerText = "Something critical went wrong. Please reload the page.";
+    return;
+  };
+
+  if ( divider === "0" ) {
+
+    console.error("Invalid divider", new Error("Divided by Zero"));
+    result.classList.add("error-message");
+    result.innerText = "Division not performed. Invalid number provided. Try again.";
   
-});
+    return;
+  };
+
+  result.innerText = divideResult;
+  });
